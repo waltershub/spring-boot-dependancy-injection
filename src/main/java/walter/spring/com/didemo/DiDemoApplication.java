dc06e88cee3502ad5340383058552ae985e0d455
Walter Shub <waltershub@gmail.com>
2019-12-20 14:33:00 -0500

    intial create project

---
+++ src/main/java/walter/spring/com/didemo/DiDemoApplication.java
@@ -0,0 +1,13 @@
+package walter.spring.com.didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiDemoApplication.class, args);
+    }
+
+}
